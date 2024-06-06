911bc9d1a53be86c6dba85b41c4f207f6ad0e131
François Rejeté <francois@rejete.com>
2024-06-06 21:16:34 +0900

    chore: Update vite.config.ts set base config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/jpn-inheritance-tax/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
